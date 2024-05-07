7bd3b9b8812a8ce2d179cc5dac25f6c6cc851c30
spmani99 <sandaleepamoda@gmail.com>
2024-05-07 18:17:33 +0530

    remove comments

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  //baseURL: "http://localhost:5000",
-  baseURL: " http://todo-app-env.eba-dgvrb8ii.ap-south-1.elasticbeanstalk.com/",
+  baseURL: "http://todo-app-env.eba-dgvrb8ii.ap-south-1.elasticbeanstalk.com/",
 });
